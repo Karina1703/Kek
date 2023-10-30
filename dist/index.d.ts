5d50bbae38272605626e161628c48cb5119d9b1c
Kabanova Karina <kabanova.karina@mail.ru>
2023-10-30 15:39:58 -0300

    Third commit

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+export declare function kek1(foo: string): string;
+declare global {
+    interface Window {
+        bee: () => void;
+    }
+}
---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.kek1 = void 0;
+function kek1(foo) {
+    return 'foo';
+}
+exports.kek1 = kek1;
